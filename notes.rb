2a63f2f25c2bef6edf6bbd9a53ebfe980aec3bc7
aausen <ausen.annie@gmail.com>
2021-12-22 11:42:27 -0600

    finish lambda review

--- notes.rb
+++ notes.rb
@@ -3,6 +3,49 @@ def greeter
     yield
   end
   
-  phrase = Proc.new { puts "Hello there!" }
-  greeter(&phrase)
+phrase = Proc.new { puts "Hello there!" }
+greeter(&phrase)
   
+
+# symbol checker using a lambda function
+my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+
+
+symbol_filter = lambda { |x| x.is_a? Symbol }
+symbols = my_array.select(&symbol_filter)
+
+puts symbols
+
+# building a block review
+odds_n_ends = [:weezard, 42, "Trady Blix", 3, true, 19, 12.345]
+
+ints = odds_n_ends.select{ |x| x.is_a? Integer }
+
+puts ints
+
+# creating a basic proc
+ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
+
+under_100 = Proc.new { |num| num < 100 }
+
+youngsters = ages.select(&under_100)
+
+puts youngsters
+
+# practice building a lambda funct 
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+
+first_half = lambda { |rank, name| name < "M"}
+
+a_to_m = crew.select(&first_half)
+
+puts a_to_m
