1e2b54341bb4b81196cae03f3dc2dd59415cb03f
aausen <ausen.annie@gmail.com>
2021-12-23 10:11:13 -0600

    inheritance practice

--- notes.rb
+++ notes.rb
@@ -49,3 +49,22 @@ first_half = lambda { |rank, name| name < "M"}
 a_to_m = crew.select(&first_half)
 
 puts a_to_m
+
+# class inheritance practice
+
+class Message
+  @@messages_sent = 0
+  def initialize(from, to)
+    @from = from
+    @to = to
+    @@messages_sent += 1
+  end
+end
+
+class Email < Message
+  def initialize(from, to)
+    super
+  end
+end
+
+my_message = Message.new("me", "you")
